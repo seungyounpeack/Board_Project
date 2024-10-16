dfe581813d3f60b820476f8be924fbebf42c1a24
seungyounpeack <bsy920521@naver.com>
2024-10-16 23:40:04 +0900

    test4 commit

---
+++ Project/src/main/java/com/board/Project/MyController.java
@@ -0,0 +1,12 @@
+package com.board.Project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public String hello() {
+        return "My name is soungyoun Peak";
+    }
+}
